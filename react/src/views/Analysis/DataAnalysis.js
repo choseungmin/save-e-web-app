fad5c2885f33df59c147d2d098357124c86362dd
seungmin <mirame@nextree.co.kr>
2019-10-30 17:10:33 +0900

    하이차트 추가

--- react/src/views/Analysis/DataAnalysis.js
+++ react/src/views/Analysis/DataAnalysis.js
@@ -1,6 +1,7 @@
 import React from "react";
 // react plugin for creating charts
 import ChartistGraph from "react-chartist";
+import ReactHighcharts from "react-highcharts";
 // react plugin for creating vector maps
 import { VectorMap } from "react-jvectormap";
 
@@ -84,6 +85,70 @@ var mapData = {
   US: 2920
 };
 
+const chartData = {
+
+  title: {
+    text: 'Solar Employment Growth by Sector, 2010-2016'
+  },
+
+  subtitle: {
+    text: 'Source: thesolarfoundation.com'
+  },
+
+  yAxis: {
+    title: {
+      text: 'Number of Employees'
+    }
+  },
+  legend: {
+    layout: 'vertical',
+    align: 'right',
+    verticalAlign: 'middle'
+  },
+
+  plotOptions: {
+    series: {
+      label: {
+        connectorAllowed: false
+      },
+      pointStart: 2010
+    }
+  },
+
+  series: [{
+    name: 'Installation',
+    data: [43934, 52503, 57177, 69658, 97031, 119931, 137133, 154175]
+  }, {
+    name: 'Manufacturing',
+    data: [24916, 24064, 29742, 29851, 32490, 30282, 38121, 40434]
+  }, {
+    name: 'Sales & Distribution',
+    data: [11744, 17722, 16005, 19771, 20185, 24377, 32147, 39387]
+  }, {
+    name: 'Project Development',
+    data: [null, null, 7988, 12169, 15112, 22452, 34400, 34227]
+  }, {
+    name: 'Other',
+    data: [12908, 5948, 8105, 11248, 8989, 11816, 18274, 18111]
+  }],
+
+  responsive: {
+    rules: [{
+      condition: {
+        maxWidth: 500
+      },
+      chartOptions: {
+        legend: {
+          layout: 'horizontal',
+          align: 'center',
+          verticalAlign: 'bottom'
+        }
+      }
+    }]
+  }
+
+}
+
 const useStyles = makeStyles({ ...styles });
 
 const DataAnalysis = (props) => {
@@ -130,12 +195,9 @@ const DataAnalysis = (props) => {
             <CardHeader color="info" icon>
             </CardHeader>
             <CardBody>
-              <ChartistGraph
-                className="ct-chart-info-colors"
-                data={dailySalesChart.data}
-                type="Line"
-                options={dailySalesChart.options}
-                listener={dailySalesChart.animation}
+              <ReactHighcharts
+                highcharts={ReactHighcharts.Highcharts}
+                config={chartData}
               />
             </CardBody>
           </Card>
@@ -145,12 +207,9 @@ const DataAnalysis = (props) => {
             <CardHeader color="info" icon>
             </CardHeader>
             <CardBody>
-              <ChartistGraph
-                className="ct-chart-info-colors"
-                data={dailySalesChart.data}
-                type="Line"
-                options={dailySalesChart.options}
-                listener={dailySalesChart.animation}
+              <ReactHighcharts
+                highcharts={ReactHighcharts.Highcharts}
+                config={chartData}
               />
             </CardBody>
           </Card>
@@ -160,12 +219,9 @@ const DataAnalysis = (props) => {
             <CardHeader color="info" icon>
             </CardHeader>
             <CardBody>
-              <ChartistGraph
-                className="ct-chart-info-colors"
-                data={dailySalesChart.data}
-                type="Line"
-                options={dailySalesChart.options}
-                listener={dailySalesChart.animation}
+              <ReactHighcharts
+                highcharts={ReactHighcharts.Highcharts}
+                config={chartData}
               />
             </CardBody>
           </Card>
@@ -175,12 +231,9 @@ const DataAnalysis = (props) => {
             <CardHeader color="info" icon>
             </CardHeader>
             <CardBody>
-              <ChartistGraph
-                className="ct-chart-info-colors"
-                data={dailySalesChart.data}
-                type="Line"
-                options={dailySalesChart.options}
-                listener={dailySalesChart.animation}
+              <ReactHighcharts
+                highcharts={ReactHighcharts.Highcharts}
+                config={chartData}
               />
             </CardBody>
           </Card>
