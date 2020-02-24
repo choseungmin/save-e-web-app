edcc01f252daf7a768135e0b2676c626e506603a
seungmin <>
2020-02-24 23:06:08 +0900

    마커 삭제하고 커스텀 오버레이로 대체  - 마커 이미지를 임의의 이미지로 사용하기 위함

--- react/src/views/Analysis/dashboard/KakaoSiteMap.js
+++ react/src/views/Analysis/dashboard/KakaoSiteMap.js
@@ -39,6 +39,10 @@ const KakaoSiteMap = (props) => {
     const bounds = new kakao.maps.LatLngBounds();
 
     for (let i = 0; i < points.length; i++) {
+      /*
+      * marker
+      * 
+
       var imageSrc = 'http://t1.daumcdn.net/localimg/localimages/07/mapapidoc/marker_red.png', // 마커이미지의 주소입니다
         imageSize = new kakao.maps.Size(48, 52), // 마커이미지의 크기입니다
         imageOption = {offset: new kakao.maps.Point(20, 40)}; // 마커이미지의 옵션입니다. 마커의 좌표와 일치시킬 이미지 안에서의 좌표를 설정합니다.
@@ -53,18 +57,20 @@ const KakaoSiteMap = (props) => {
         image: markerImage // 마커이미지 설정
       });
       marker.setMap(kakaoMap);
+      */
 
       // 커스텀 오버레이에 표출될 내용으로 HTML 문자열이나 document element가 가능합니다
       var content = `<div class="customoverlay">
-        <span class="title">${(i+1)}</span>
-      </div>`;
+                         <div class="marker">
+                             <span class="title">${(i+1)}</span>
+                         </div>
+                     </div>`;
 
       // 커스텀 오버레이를 생성합니다
       var customOverlay = new kakao.maps.CustomOverlay({
         map: kakaoMap,
         position: points[i],
         content: content,
-        yAnchor: 1
       });
       // LatLngBounds 객체에 좌표를 추가합니다
       bounds.extend(points[i]);
